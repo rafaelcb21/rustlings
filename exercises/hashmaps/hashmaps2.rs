0da216702fff65b7fa0486a1f7e82042e2147549
rafael-1686957388 <rafaelcb21@gmail.com>
2023-07-10 03:17:15 -0300

    fazendo options

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -28,18 +28,25 @@ enum Fruit {
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+    //let fruit_kinds = vec![
+    //    Fruit::Banana,
+    //    Fruit::Pineapple,
+    //];
+
+    //for fruit in fruit_kinds {
+    //    basket.insert(fruit, 5);
+    //}
+
     let fruit_kinds = vec![
         Fruit::Apple,
         Fruit::Banana,
         Fruit::Mango,
         Fruit::Lychee,
-        Fruit::Pineapple,
+        Fruit::Pineapple
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        basket.entry(fruit).or_insert(2);
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -34,11 +34,33 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        
+        scores
+            .entry(team_1_name) // consegue verificar se a chave existe ou não
+            .and_modify(|team| { // se a chave existir permite modificar a entrada existente
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(
+                Team{
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score
+                }
+            );
+        
+        scores
+            .entry(team_2_name) // consegue verificar se a chave existe ou não
+            // |team| é uma referencia para a chave
+            .and_modify(|team| { // se a chave existir permite modificar a entrada existente
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert( // insere o valor, não a chave. A chave será inserina no entry
+                Team{
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score
+                }
+            );
     }
     scores
 }
