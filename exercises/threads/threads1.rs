3e5e6889f1cf9d2405c5de58fd5c5bd1fb60e626
rafael-1686957388 <rafaelcb21@gmail.com>
2023-08-06 22:54:18 -0300

    threads and macros

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,7 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -25,8 +25,10 @@ fn main() {
     }
 
     let mut results: Vec<u128> = vec![];
+    let mut count = 0;
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        results.push(count);
+        count += 1;
     }
 
     if results.len() != 10 {
